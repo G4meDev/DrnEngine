4040d8f4de50d612cfc4c298bdab94a867d5829e
G4meD3v <mrwpgg@gmail.com>
2026-07-26 15:10:58 +0330

    add sparks particle

---
+++ Engine/Content/StarterContent/Particles/Materials/M_Sparks.hlsl
@@ -0,0 +1,71 @@
+#include "Common.hlsl"
+
+// DOMAIN_SURFACE
+// BLEND_TRANSLUCENT
+// SHADING_UNLIT
+
+// SUPPORT_PARTICLE_SPRITE
+// SUPPORT_MAIN_PASS
+
+ConstantBuffer<TranslucentResources> BindlessResources : register(b0);
+
+struct VertexShaderOutput
+{
+    float4 Color : COLOR;
+    float2 UV : UV;
+    float4 Position : SV_Position;
+};
+
+struct PixelShaderOutput
+{
+#if TRANSLUCENCY_PASS
+    float4 TranslucentColor;
+#endif
+};
+
+VertexShaderOutput Main_VS(VertexInputParticleSprite IN)
+{
+    VertexShaderOutput OUT;
+    
+    ConstantBuffer<ViewBuffer> View = ResourceDescriptorHeap[BindlessResources.ViewIndex];
+    ConstantBuffer<ParticleSpriteBuffer> ParticleBuffer = ResourceDescriptorHeap[BindlessResources.PrimitiveIndex];
+
+    float4 WorldPosition = mul(ParticleBuffer.SimulationToWorld, float4(IN.ParticlePosition_RelativeTime.xyz, 1.0f));
+    float4 OldWorldPosition = mul(ParticleBuffer.SimulationToWorld, float4(IN.ParticleOldPosition_Id.xyz, 1.0f));
+    
+    float3 Right, Up;
+    ParticleSpriteTangents(View, IN.Size_Rotation_Subindex.z, WorldPosition.xyz, OldWorldPosition.xyz, Right, Up, true);
+    
+    float2 Size = abs(IN.Size_Rotation_Subindex.xy);
+    WorldPosition.xyz += Size.x * (IN.Position.x - 0.5f) * Right;
+    WorldPosition.xyz += Size.y * (IN.Position.y - 0.5f) * Up;
+    
+    float3x3 TangentToWorld = ParticleSpriteCalcTangentBasis(Right, Up);
+    
+    OUT.Position = mul(View.WorldToProjection, WorldPosition);
+    OUT.Color = IN.ParticleColor;
+    OUT.UV = IN.Position;
+    
+    return OUT;
+}
+
+// -------------------------------------------------------------------------------------
+
+struct PixelShaderInput
+{
+    float4 Color : COLOR;
+    float2 UV : UV;
+};
+
+PixelShaderOutput Main_PS(PixelShaderInput IN) : SV_Target
+{
+    float4 OutColor = float4(IN.Color.rgb, IN.Color.a * RadialGradientExponential(IN.UV));
+    
+    PixelShaderOutput OUT;
+    
+#if TRANSLUCENCY_PASS
+    OUT.TranslucentColor = OutColor;
+#endif
+    
+    return OUT;
+}
