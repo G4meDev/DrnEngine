aa53504efe0a785ff32199009d4ef9aadfba2cd1
G4meD3v <mrwpgg@gmail.com>
2026-07-27 16:26:51 +0330

    adding more fx

---
+++ Engine/Content/StarterContent/Particles/Materials/M_Explosion.hlsl
@@ -0,0 +1,102 @@
+#include "Common.hlsl"
+
+// DOMAIN_SURFACE
+// BLEND_TRANSLUCENT
+// SHADING_LIT
+
+// SUPPORT_PARTICLE_SPRITE
+// SUPPORT_MAIN_PASS
+
+ConstantBuffer<TranslucentResources> BindlessResources : register(b0);
+
+struct ParametersBuffers
+{
+    TEX2D(ExplosionSubuv, ExplosionSubuvTexture)
+};
+
+struct VertexShaderOutput
+{
+    float4 Color : COLOR;
+    float2 UV : UV;
+    float SubImage : SUBUV;
+    float4 Position : SV_Position;
+};
+
+struct PixelShaderOutput
+{
+#if TRANSLUCENCY_PASS
+    float4 TranslucentColor;
+#endif
+};
+
+VertexShaderOutput Main_VS(VertexInputParticleSprite IN)
+{
+    VertexShaderOutput OUT;
+    
+    ConstantBuffer<ViewBuffer> View = ResourceDescriptorHeap[BindlessResources.ViewIndex];
+    ConstantBuffer<ParticleSpriteBuffer> ParticleBuffer = ResourceDescriptorHeap[BindlessResources.PrimitiveIndex];
+
+    float4 WorldPosition = mul(ParticleBuffer.SimulationToWorld, float4(IN.ParticlePosition_RelativeTime.xyz, 1.0f));
+    float4 OldWorldPosition = mul(ParticleBuffer.SimulationToWorld, float4(IN.ParticleOldPosition_Id.xyz, 1.0f));
+    
+    float3 Right, Up;
+    ParticleSpriteTangents(View, IN.Size_Rotation_Subindex.z, WorldPosition.xyz, OldWorldPosition.xyz, Right, Up, false);
+
+    float2 Size = abs(IN.Size_Rotation_Subindex.xy);
+    WorldPosition.xyz += Size.x * (IN.Position.x - 0.5f) * Right;
+    WorldPosition.xyz += Size.y * (IN.Position.y - 0.5f) * Up;
+
+    OUT.Position = mul(View.WorldToProjection, WorldPosition);
+    OUT.Color = IN.ParticleColor;
+    OUT.UV = IN.Position;
+    OUT.SubImage = IN.Size_Rotation_Subindex.w;
+    
+    return OUT;
+}
+
+// -------------------------------------------------------------------------------------
+
+struct PixelShaderInput
+{
+    float4 Color : COLOR;
+    float2 UV : UV;
+    float SubImage : SUBUV;
+    float4 Position : SV_Position;
+};
+
+PixelShaderOutput Main_PS(PixelShaderInput IN) : SV_Target
+{
+    ConstantBuffer<ParametersBuffers> Parameters = ResourceDescriptorHeap[BindlessResources.ParametersBufferIndex];
+    ConstantBuffer<ViewBuffer> View = ResourceDescriptorHeap[BindlessResources.ViewIndex];
+    ConstantBuffer<StaticSamplers> StaticSamplers = ResourceDescriptorHeap[BindlessResources.StaticSamplerBufferIndex];
+    ConstantBuffer<GBufferTextures> GbufferTextures = ResourceDescriptorHeap[BindlessResources.GbufferTextureIndex];
+    
+    SamplerState LinearSampler = ResourceDescriptorHeap[StaticSamplers.LinearSamplerIndex];
+    SamplerState PointSampler = ResourceDescriptorHeap[StaticSamplers.PointClampIndex];
+    
+    float2 ScreenUV = SvPositionToViewportUV(IN.Position.xy, View.InvSize);
+    Texture2D DepthTexture = ResourceDescriptorHeap[GbufferTextures.DepthIndex];
+    float SceneDepth = ConvertFromDeviceZ(DepthTexture.Sample(PointSampler, ScreenUV).r, View.InvDeviceZToWorldZTransform);
+    float PixelDepth = ConvertFromDeviceZ(IN.Position.z, View.InvDeviceZToWorldZTransform);
+    
+    Texture2D ExplosionSubuvTexture = ResourceDescriptorHeap[Parameters.ExplosionSubuv_Texture];
+    SamplerState ExplosionSubuvSampler = ResourceDescriptorHeap[Parameters.ExplosionSubuv_Sampler];
+    
+    float4 ExplosionSubuv = TextureSubuv(ExplosionSubuvTexture, ExplosionSubuvSampler, IN.UV, float2(6, 6), IN.SubImage, true);
+    
+    //float Opacity = saturate(IN.Color.a * SmokeSubuv.a);
+    float Opacity = saturate(IN.Color.a);
+    Opacity = DepthFade(SceneDepth, PixelDepth, Opacity, 0.6f);
+    
+    float3 Color = IN.Color.rgb * ExplosionSubuv.rgb;
+    
+    float4 OutColor = float4(Color, Opacity);
+    
+    PixelShaderOutput OUT;
+    
+#if TRANSLUCENCY_PASS
+    OUT.TranslucentColor = OutColor;
+#endif
+    
+    return OUT;
+}
