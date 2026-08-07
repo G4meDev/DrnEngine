929ed1325329e24567c321db39a51445671d894a
G4meD3v <mrwpgg@gmail.com>
2026-08-07 11:55:31 +0330

    add more to level 3

--- Engine/Source/ForwardTypes.h
+++ Engine/Source/ForwardTypes.h
@@ -116,8 +116,8 @@ class LogCategory;
 #define LOG_DECLARE_CATEGORY(Category)					\
 	extern LogCategory Category;						\
 
-#define D3D12_DEBUG_LAYER 1
-#define D3D12_GPU_VALIDATION 1
+#define D3D12_DEBUG_LAYER 0
+#define D3D12_GPU_VALIDATION 0
 #define D3D12_DRED 0
 #define D3D12_Debug_INFO 1
 #define RENDER_STATS 1
